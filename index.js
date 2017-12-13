2ce0cf4e95d303fe452eff10723cddbfa81d1833
Mick McGrath <mick.mc.13@gmail.com>
2017-12-13 09:04:21 -0600

    updated values to appear more metric-y

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ var prettyjson = require('prettyjson');
 program
     .arguments('<command>')
     .option('-l, --location <location>', 'The failure location')
-    .option('-n, --notify <email>', 'The email to send a notification')
     .action(function(command) {
         fs.readFile(`${command}.json`, 'utf8', function (err, data) {
             if (err) throw err;
             console.log(JSON.stringify(JSON.parse(data)));
-            if (program.notify) {
-                console.log(`Emailing ${program.notify} with the above diagnostic data`);
-            }
         });
     })
     .parse(process.argv);
